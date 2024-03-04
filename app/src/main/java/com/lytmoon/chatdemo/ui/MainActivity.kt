4e091437baf4a4e4196a6498682c99efc06e4d53
lytMoon <2936437824@qq.com>
2024-03-04 16:18:38 +0800

    :fire: 正式版v1初步调整完成，打包发布

--- app/src/main/java/com/lytmoon/chatdemo/ui/MainActivity.kt
+++ app/src/main/java/com/lytmoon/chatdemo/ui/MainActivity.kt
@@ -126,7 +126,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun iniTrans() {
         window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         window.statusBarColor = Color.TRANSPARENT
     }
 
